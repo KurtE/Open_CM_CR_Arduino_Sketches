39b884d443bb7fff027609aeca738bb254e2d128
Kurt Eckhardt <kurte@rockisland.com>
2018-10-04 07:07:23 -0700

    Basic Servo controller: first attempt follow USB Baud changes
    First pass at allowing the Servo controller code detect when the Host changes the desired baud rate and then update the DXL Serial bus to the new speed.
    
    This worked with Dynamixel Manager, to detect at different speeds, it even went to the servo firmware recovery setup and detected the servo, but for some reason the firmware recovery fails... Still debugging

--- OpenCM_Basic_Servo_Controller/OpenCM_Basic_Servo_Controller.ino
+++ OpenCM_Basic_Servo_Controller/OpenCM_Basic_Servo_Controller.ino
@@ -1,13 +1,21 @@
 #include <DynamixelSDK.h>
 //====================================================================================================
-// Really basic Servo Controller... 
+// Really basic Servo Controller...
 // This version for Robotis OpenCM9.04
 //====================================================================================================
 //=============================================================================
 // Options...
 //=============================================================================
 #define BAUDRATE                        1000000
+#define DEVICE 1
+#if DEVICE
 #define DEVICENAME                      "3"                 //DEVICENAME "1" -> Serial1(OpenCM9.04 DXL TTL Ports)
+#define DXLSerial Serial3
+#else
+#define DEVICENAME                      "1"                 //DEVICENAME "1" -> Serial1(OpenCM9.04 DXL TTL Ports)
+#define DXLSerial Serial1
+#endif
+
 #define BUFFER_SIZE   64
 //=============================================================================
 // Globals
@@ -15,20 +23,25 @@
 dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler(DEVICENAME);
 uint8_t buffer[BUFFER_SIZE];
 int led_state = LOW;
+uint32_t g_baud_rate = BAUDRATE;
 
+extern USBD_CDC_LineCodingTypeDef LineCoding;
 //====================================================================================================
 // Setup
 //====================================================================================================
 void setup() {
   pinMode(BOARD_LED_PIN, OUTPUT);
-
-  Serial.begin(115200); // actual speed does not matter USB
-
+  Serial2.begin(115200);
+  Serial.begin(BAUDRATE); // Init to actual default baud
+  Serial2.println("Start Basic Servo Controller");
   // Open Port handler
   portHandler = dynamixel::PortHandler::getPortHandler(DEVICENAME);
   if (!portHandler->openPort()) signal_abort(1);
   if (!portHandler->setBaudRate(BAUDRATE)) signal_abort(2);
+
+  Serial2.printf("LineCoding bitrate: %d\n", LineCoding.bitrate);
 }
+
 
 //====================================================================================================
 // loop
@@ -39,7 +52,7 @@ void loop() {
   // First forward anything received from DXL
   cb = portHandler->readPort(buffer, BUFFER_SIZE);
   if (cb > 0) {
-    Serial.write(buffer, cb);  
+    Serial.write(buffer, cb);
     did_something = true;
   }
 
@@ -49,14 +62,23 @@ void loop() {
     buffer[cb++] = Serial.read();
   }
   if (cb) {
-    portHandler->writePort(buffer, cb);  
+    portHandler->writePort(buffer, cb);
     did_something = true;
   }
   if (did_something) {
-    led_state = led_state? LOW : HIGH;
+    led_state = led_state ? LOW : HIGH;
     digitalWrite(BOARD_LED_PIN, led_state);
   }
-  
+
+  // lets see if USB baudrate changed?
+  //  uint32_t new_baud_rate = Serial.getBaudRate();
+  uint32_t new_baud_rate = LineCoding.bitrate;
+  if (new_baud_rate != g_baud_rate) {
+    DXLSerial.begin(new_baud_rate);
+    Serial2.print("New Baud Rate: ");
+    Serial2.println(new_baud_rate, DEC);
+    g_baud_rate = new_baud_rate;
+  }
 }
 
 
