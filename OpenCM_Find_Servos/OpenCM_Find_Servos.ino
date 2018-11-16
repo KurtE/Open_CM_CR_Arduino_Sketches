0db5b3656bef7b16cc616b68d4f7a7a059174c8b
Kurt Eckhardt <kurte@rockisland.com>
2018-11-15 19:02:12 -0800

    Find Servos - Play with broadcast ping

--- OpenCM_Find_Servos/OpenCM_Find_Servos.ino
+++ OpenCM_Find_Servos/OpenCM_Find_Servos.ino
@@ -1,11 +1,8 @@
 #include <DynamixelSDK.h>
-
-
 //====================================================================================================
-// Kurts Test program to try out different ways to manipulate the AX12 servos on the PhantomX
+// Kurts Quick and dirty test program to find servos on either Open CM or Open CR boards
 // This is a test, only a test...
 //
-// This version for Robotis OpenCM9.04
 //====================================================================================================
 //============================================================================
 // Global Include files
@@ -41,124 +38,14 @@ uint8_t port_handler_numbers[] = {1};     // Default to Serial1
 #define PROTOCOL_VERSION2                2.0                 // See which protocol version is used in the Dynamixel
 #define DEVICENAME                      "3"                 // Check which port is being used on your controller
 #define DXL_BAUDRATE 1000000
+
 /** EEPROM AREA **/
-#define AX_MODEL_NUMBER_L           0
-#define AX_MODEL_NUMBER_H           1
-#define AX_VERSION                  2
-#define AX_ID                       3
-#define AX_BAUD_RATE                4
-#define AX_RETURN_DELAY_TIME        5
-#define AX_CW_ANGLE_LIMIT_L         6
-#define AX_CW_ANGLE_LIMIT_H         7
-#define AX_CCW_ANGLE_LIMIT_L        8
-#define AX_CCW_ANGLE_LIMIT_H        9
-#define AX_SYSTEM_DATA2             10
-#define AX_LIMIT_TEMPERATURE        11
-#define AX_DOWN_LIMIT_VOLTAGE       12
-#define AX_UP_LIMIT_VOLTAGE         13
-#define AX_MAX_TORQUE_L             14
-#define AX_MAX_TORQUE_H             15
-#define AX_RETURN_LEVEL             16
-#define AX_ALARM_LED                17
-#define AX_ALARM_SHUTDOWN           18
-#define AX_OPERATING_MODE           19
-#define AX_DOWN_CALIBRATION_L       20
-#define AX_DOWN_CALIBRATION_H       21
-#define AX_UP_CALIBRATION_L         22
-#define AX_UP_CALIBRATION_H         23
-/** RAM AREA **/
-#define AX_TORQUE_ENABLE            24
-#define AX_LED                      25
-#define AX_CW_COMPLIANCE_MARGIN     26
-#define AX_CCW_COMPLIANCE_MARGIN    27
-#define AX_CW_COMPLIANCE_SLOPE      28
-#define AX_CCW_COMPLIANCE_SLOPE     29
-#define AX_GOAL_POSITION_L          30
-#define AX_GOAL_POSITION_H          31
-#define AX_GOAL_SPEED_L             32
-#define AX_GOAL_SPEED_H             33
-#define AX_TORQUE_LIMIT_L           34
-#define AX_TORQUE_LIMIT_H           35
 #define AX_PRESENT_POSITION_L       36
-#define AX_PRESENT_POSITION_H       37
-#define AX_PRESENT_SPEED_L          38
-#define AX_PRESENT_SPEED_H          39
-#define AX_PRESENT_LOAD_L           40
-#define AX_PRESENT_LOAD_H           41
-#define AX_PRESENT_VOLTAGE          42
-#define AX_PRESENT_TEMPERATURE      43
-#define AX_REGISTERED_INSTRUCTION   44
-#define AX_PAUSE_TIME               45
-#define AX_MOVING                   46
-#define AX_LOCK                     47
-#define AX_PUNCH_L                  48
-#define AX_PUNCH_H                  49
 //=============================================================
 // Defines for X series (XL430)
 //=============================================================================
-#define DXL_PING                    1
-#define DXL_READ_DATA               2
-#define DXL_WRITE_DATA              3
-#define DXL_REG_WRITE               4
-#define DXL_ACTION                  5
-#define DXL_RESET                   6
-#define DXL_SYNC_READ               0x82
-#define DXL_SYNC_WRITE              0x83
-
-
-/** EEPROM AREA **/
-#define DXL_X_MODEL_NUMBER         0 //2 1060(xl430-250)
-#define DXL_X_MODEL_INFORMATION    2 //4
-#define DXL_X_VERSION              6
-#define DXL_X_ID                   7   // 1 1
-#define DXL_X_BAUD_RATE            8   // 1 1
-#define DXL_X_RETURN_DELAY_TIME    9   // 1 250
-#define DXL_X_DRIVE_MODE           10  // 1 0
-#define DXL_X_OPERATING_MODE       11  // 1 3
-#define DXL_X_SECONDARY_ID         12  // 1 255
-#define DXL_X_PROTOCOL_VERSION     13  // 1 - 2
-#define DXL_X_HOMING_OFFSET        20  // 4 - 0
-#define DXL_X_MOVING_THRESHOLD     24  // 4 - 10
-#define DXL_X_TEMPERATURE_LIMIT    31  //1 - 72
-#define DXL_X_MAX_VOLTAGE_LIMIT    32  // 2 140
-#define DXL_X_MIN_VOLTAGE_LIMIT    34  // 2 60
-#define DXL_X_PWM_LIMIT            36  // 2 885
-#define DXL_X_ACCELERATION_LIMIT   40  // 4 32767
-#define DXL_X_VElOCITY_LIMIT       44  // 4 415
-#define DXL_X_MAX_POSITION_LIMIT   48  // 4 4095
-#define DXL_X_MIN_POSITION_LIMIT   52  // 4 0
-#define DXL_X_SHUTDOWN             63  // 1 52
-
-/** RAM AREA **/
-#define DXL_X_TORQUE_ENABLE        64  // 1 0
-#define DXL_X_LED                  65  // 1 0
-#define DXL_X_STATUS_RETURN_LEVEL  68  // 1 2
-#define DXL_X_REGISTERED_INSTRUCTION 69 //1 (R)
-#define DXL_X_HARDWARE_ERROR_STATUS  70 // 1 (R)
-#define DXL_X_VELOCITY_I_GAIN      76  // 2 1000 
-#define DXL_X_VELOCITY_P_GAIN      78  // 2 100 
-#define DXL_X_POSITION_D_GAIN      80  // 2 4000 
-#define DXL_X_POSITION_I_GAIN      82  // 2 0
-#define DXL_X_POSITION_P_GAIN      84  // 2 640
-#define DXL_X_FEEDFORWARD_2_GAIN   88  // 2 0
-#define DXL_X_FEEDFORWARD_1_GAIN   90  // 2 0
-#define DXL_X_BUS_WATCHDOG         98  // 1 -
-#define DXL_X_GOAL_PWM             100 // 2 
-#define DXL_X_GOAL_VELOCITY        104 // 4
-#define DXL_X_PROFILE_ACCELERATION 108 // 4
-#define DXL_X_PROFILE_VELOCITY     112 // 4
-#define DXL_X_GOAL_POSITION        116 // 4
-#define DXL_X_REALTIME_TICK        120 // 2 (R)
-#define DXL_X_MOVING               122 // 1 (R)
-#define DXL_X_MOVING_STATUS        123 // 1 (R)
-#define DXL_X_PRESENT_PWN          124 // 2 (R)
-#define DXL_X_PRESENT_LOAD         126 // 2 (R)
-#define DXL_X_PRESENT_VELOCITY     128 // 4 (R)
+#define DXL_X_MODEL_NUMBER         0    // 2 (R)
 #define DXL_X_PRESENT_POSITION     132 // 4 (R)
-#define DXL_X_VELOCITY_TRAJECTORY  136 // 4 (R)
-#define DXL_X_POSITION_TRAJECTORY  140 // 4 (R)
-#define DXL_X_PRESENT_INPUT_VOLTAGE  144 // 2 (R)
-#define DXL_X_PRESENT_TEMPERATURE  146 // 1 (R)
 
 
 
@@ -181,7 +68,6 @@ void setup() {
   while (!Serial && (millis() < 3000)) ;  // Give time for Teensy and other USB arduinos to create serial port
   Serial.begin(38400);  // start off the serial port.
   Serial.println("\nCM9.04 Find Servos program");
-
 #if defined(SERVO_RX_PIN)
   SERVOBUS.setRX(SERVO_RX_PIN);
 #endif
@@ -269,6 +155,7 @@ void FindServos(void) {
     Serial.println(port_handler_numbers[port_index], DEC);
 
     Serial.println("  Begin Protocol 1: ");
+    digitalWrite(4, !digitalRead(4));
     for (int i = 0; i < 254; i++) {
       if (packetHandler1->read2ByteTxRx(portHandler, i, AX_PRESENT_POSITION_L, &w) == COMM_SUCCESS) {
         if (g_servo_protocol[i]) {
@@ -284,6 +171,7 @@ void FindServos(void) {
 
     Serial.println("  Done");
     Serial.println("  Begin Protocol 2: ");
+    digitalWrite(4, !digitalRead(4));
     for (int i = 0; i < 254; i++) {
       uint16_t model_number;
       uint32_t position;
@@ -301,6 +189,27 @@ void FindServos(void) {
         Serial.println(position, DEC);
       }
     }
+    Serial.println("  Try Protocol 2 - broadcast ping: ");
+    digitalWrite(4, !digitalRead(4));
+    
+    std::vector<uint8_t> vec;                       // Dynamixel data storages
+    if (packetHandler2->broadcastPing(portHandler, vec) == COMM_SUCCESS) {
+      Serial.print("Detected Dynamixel : \n");
+      for (int i = 0; i < (int)vec.size(); i++)
+      {
+        int id = vec.at(i);
+        Serial.print("    ");
+        Serial.print(id, DEC);
+        uint16_t model_number = 0;
+        packetHandler2->read2ByteTxRx(portHandler, id, DXL_X_MODEL_NUMBER, &model_number);
+        Serial.print(", Model:");
+        Serial.println((int)model_number, HEX);
+        
+        
+        g_servo_protocol[i] = 2;
+      }
+    } else Serial.println("Broadcast ping failed");
+    
     Serial.println("  Done");
   }
 }
